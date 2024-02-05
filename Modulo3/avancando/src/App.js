93a1341794e77ed40c5ee619a96683248a0cb625
Olivio Neto <netoolivio34@gmail.com>
2024-02-05 11:00:01 -0300

    Renderizando listas em componentes

--- Modulo3/avancando/src/App.js
+++ Modulo3/avancando/src/App.js
@@ -13,6 +13,12 @@ function App() {
   const name = "Joaquim"
   const [userName] = useState("Maria")
 
+  const cars = [
+    {id:1, brand: "Ferrari", color: "Amarela", newCar: true, km:0},
+    {id:2, brand: "KIA", color: "Branco", newCar: false, km:34343},
+    {id:3, brand: "Renault", color: "Azul", newCar: false, km:234}
+  ]
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -39,6 +45,15 @@ function App() {
       {/* reaproveitamento */}
       <CarDetails brand="Ford" color="Vermelha" km={0} newCar={true}/>
       <CarDetails brand="Fiat" color="Branco" km={4500} newCar={false} />
+      {/* loop em array de objetos */}
+      {cars.map((car) =>(
+        <CarDetails 
+          brand={car.brand} 
+          color={car.color} 
+          km={car.km} 
+          newCar={car.newCar} 
+        />
+      ))}
     </div>
   );
 }
