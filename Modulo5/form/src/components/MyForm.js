75cf696ba6495485fcb10d10fa5a7671b30a7939
Olivio Neto <netoolivio34@gmail.com>
2024-03-07 09:50:55 -0300

    Outra forma de fazer input

--- Modulo5/form/src/components/MyForm.js
+++ Modulo5/form/src/components/MyForm.js
@@ -10,7 +10,11 @@ const MyForm = () => {
                 <label htmlFor="name">Nome:</label>
                 <input type="text" name="name" placeholder="Digite o seu nome"/>
             </div>
-            {/*  */}
+            {/* 2 - label envolvendo input */}
+            <label>
+                <span>E-mail</span>
+                <input type="email" name="email" placeholder="Digite o seu e-mail"/>
+            </label>
             <input type="submit" value="Enviar"/>
         </form>
     </div>
